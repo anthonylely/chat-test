081e42578590c0eec385e74db4c8342ed46d62fe
folklorique <anthony.lely@me.com>
2015-11-25 18:02:10 +0100

    Scaffold message

---
+++ app/views/messages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @message, :id, :content, :read_at, :sender_id, :recipient_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125163230) do
+ActiveRecord::Schema.define(version: 20151125165445) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "read_at"
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
